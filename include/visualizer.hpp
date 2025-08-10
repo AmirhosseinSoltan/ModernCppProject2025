b151b7a6c62699c42c168ea941f35ea5d9ec0942
Amirhossein <ah.soltani2010@gmail.com>
2025-08-10 19:28:51 +0200

    Add visualize function for point cloud rendering: Introduced a new header file to facilitate visualization of point clouds using Open3D, enhancing the project's graphical capabilities.

---
+++ include/visualizer.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Eigen/Core>
+#include <memory>
+#include <vector>
+#include <iostream>
+
+#include "open3d/Open3D.h"
+
+inline void visualize(
+        const std::vector<Eigen::Vector3d>& pointcloud, const std::string& name) {
+    std::cout << "Creating PointCloud with " << pointcloud.size() << " points" << std::endl;
+    
+    auto pcd = std::make_shared<open3d::geometry::PointCloud>();
+    
+    for (size_t i = 0; i < pointcloud.size(); i++) {
+        const auto& pt = pointcloud[i];
+        pcd->points_.emplace_back(pt.x(), pt.y(), pt.z());
+    }
+    
+    // Add bright red color to make points more visible
+//     pcd->colors_.resize(pcd->points_.size(), Eigen::Vector3d(1.0, 0.0, 0.0));
+    
+
+    open3d::visualization::DrawGeometries({pcd}, name, 1600, 1024);
+}
