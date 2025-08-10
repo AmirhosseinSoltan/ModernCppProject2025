89e57309753a29e430e36bf13b2a8edc5ba1694e
Amirhossein <ah.soltani2010@gmail.com>
2025-08-10 19:46:45 +0200

    Remove dataloader and visualizer modules: Deleted dataloader.cpp, dataloader.hpp, and visualizer.hpp files, eliminating point cloud data handling and visualization functionalities from the project.

--- src/transformer.cpp
+++ src/transformer.cpp
@@ -4,17 +4,21 @@
 #include "dataloader.hpp"
 #include "transformer.hpp"
 #include <algorithm>
+using namespace std;
 
 namespace transformer {
     Vector3dVector TransformDataset(const dataloader::Dataset& dataset) {
         
+        cout << "TRANSFORMING POINTS TO REFRENCE FRAME ... " << endl;
         //output cloud
         Vector3dVector transformed_cloud;
         transformed_cloud.reserve(dataset.size() * 64000); // rough upper bound
-        // for (int i = 0; i < dataset.size(); ++i) {
-        for (int i = 0; i < 10; ++i) {
-            const auto& [pose, cloud] = dataset[i];
 
+        // for (int i = 0; i < dataset.size(); ++i) {
+        for (int i = 0; i < 500; ++i) {
+            const auto& [pose, cloud] = dataset[i];
+            
+            //lambda
             auto homogenous_transform = [&](const Eigen::Vector3d& point) {
                 Eigen::Vector4d hom(point.x(), point.y(), point.z(), 1.0);
                 transformed_cloud.emplace_back((pose * hom).head<3>());
@@ -25,6 +29,8 @@ namespace transformer {
         }
 
         transformed_cloud.shrink_to_fit();
+
+        cout << " FINISHED TRANSFORMATION." << endl;
         return transformed_cloud;
     }
 }
