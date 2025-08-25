ee3f05d3d03f9c26ff48278fa20629bb659a9a48
Amirhossein <ah.soltani2010@gmail.com>
2025-08-25 21:32:31 +0200

    Refactor bresenham.hpp to enhance code clarity by adding namespace qualifiers, updating function signatures, and improving type consistency.

--- include/bresenham.hpp
+++ include/bresenham.hpp
@@ -6,10 +6,11 @@
 #include <tuple>
 #include <cmath>
 
-using namespace std;
-using Vector3dVector = vector<Eigen::Vector3d>;
+using std::cout;
+using std::endl;
+using std::size_t;
 using Vector3d = Eigen::Vector3d;
-
+using Vector3dVector = std::vector<Vector3d>;
 
 struct VoxelKey {
     int x;
@@ -37,21 +38,21 @@ class OccupancyGrid3D {
         double voxel_size_;
         inline VoxelKey toVoxelKey(const Vector3d& pt) const {
             return {
-                static_cast<int>(floor(pt.x() / voxel_size_)),
-                static_cast<int>(floor(pt.y() / voxel_size_)),
-                static_cast<int>(floor(pt.z() / voxel_size_))
+                static_cast<int>(std::floor(pt.x() / voxel_size_)),
+                static_cast<int>(std::floor(pt.y() / voxel_size_)),
+                static_cast<int>(std::floor(pt.z() / voxel_size_))
             };
         }
-        inline double probToLogOdds(double p) const {return log(p / (1.0 - p));} // probability --->  log-odds
-        inline double logOddsToProb(double l) const {return 1.0 - (1.0 / (1.0 + exp(l)));} // log-odds --> probability
         
-        unordered_map<VoxelKey, double, VoxelHash> occupancy_map_; // Occupancy stored as : { key : log-odds values }
+        std::unordered_map<VoxelKey, double, VoxelHash> occupancy_map_; // Occupancy stored as : { key : log-odds values }
 
-        void updateVoxelProbability(const VoxelKey& key, double log_odds_update);
+        void updateVoxelProbability(const VoxelKey& key, double& log_odds_update);
 
     public:
-        OccupancyGrid3D(double voxel_size) : voxel_size_(voxel_size) {}
+        static inline double probToLogOdds(double p) { return std::log(p / (1.0 - p)); } // probability --->  log-odds
+        static inline double logOddsToProb(double l) { return 1.0 - (1.0 / (1.0 + std::exp(l))); } // log-odds --> probability
+        OccupancyGrid3D(const double& voxel_size) : voxel_size_(voxel_size) {};
         void insertRay(const Vector3d& start, const Vector3d& end);
-        Vector3dVector getOccupiedPoints(double occ_threshold) const;
+        Vector3dVector getOccupiedPoints(const double& occ_threshold);
    
-    };
+};
